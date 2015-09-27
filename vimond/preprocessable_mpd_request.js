dc815dac9e3c3488d4600284ab4c7fb90c10a06e
Tor Erik Alræk <torerik@vimond.com>
2015-09-27 22:22:37 +0200

    Added optional extraction/application of missing presentationTimeOffset.

--- vimond/preprocessable_mpd_request.js
+++ vimond/preprocessable_mpd_request.js
@@ -29,6 +29,8 @@ shaka.vimond.dash.PreprocessableMpdRequest = function(url, opt_requestTimeout, o
     shaka.dash.MpdRequest.call(this, url, opt_requestTimeout);
     /** @private {shaka.vimond.dash.ManifestTextPreprocessor} */
     this.manifestTextPreprocessor_ = new shaka.vimond.dash.ManifestTextPreprocessor(opt_modificationSetup);
+    /** @private {?shaka.vimond.dash.ManifestModificationSetup} */
+    this.modificationSetup_ = opt_modificationSetup || null;
 };
 
 goog.inherits(shaka.vimond.dash.PreprocessableMpdRequest, shaka.dash.MpdRequest);
@@ -39,11 +41,75 @@ shaka.vimond.dash.PreprocessableMpdRequest.prototype.send = function() {
     return url.fetch(this.parameters_).then(function(data) {
             var mpd = shaka.dash.mpd.parseMpd(this.manifestTextPreprocessor_.process(data), url.urls);
             if (mpd) {
-                return Promise.resolve(mpd);
+                return Promise.resolve(this.applyPresentationTimeOffsetFix_(mpd));
             }
 
             var error = new Error('MPD parse failure.');
             error.type = 'mpd';
             return Promise.reject(error);
         }.bind(this));
+};
+
+/**
+ * If a presentationTimeOffset fix policy is specified, applies such a fix if needed, by mutating the Mpd instance.
+ * @param {shaka.dash.mpd.Mpd} mpd
+ * @returns {shaka.dash.mpd.Mpd}
+ */
+shaka.vimond.dash.PreprocessableMpdRequest.prototype.applyPresentationTimeOffsetFix_ = function(mpd) {
+    "use strict";
+    if (mpd.type === 'static' && mpd.periods && this.modificationSetup_ && this.modificationSetup_.presentationTimeOffsetFixPolicy && this.modificationSetup_.presentationTimeOffsetFixPolicy == 'firstVideo') {
+        mpd.periods.forEach(function(/** {shaka.dash.mpd.Period} */ period) {
+            var offset = this.findFirstOffsetWithVideo_(period);
+            try {
+                if (offset > 0) {
+                    shaka.log.info('Found missing presentationTimeOffset from segment start offset, based on the configured ' + this.modificationSetup_.presentationTimeOffsetFixPolicy + ' policy.', offset);
+                    period.adaptationSets.forEach(function (a) {
+                        if (!a.segmentTemplate.presentationTimeOffset) {
+                            a.segmentTemplate.presentationTimeOffset = offset * (a.segmentTemplate.timescale || 1);
+                        }
+                        if (a.representations) {
+                            a.representations.forEach(function(/** {shaka.dash.mpd.Representation} */ r) {
+                                if (r.segmentTemplate && !r.segmentTemplate.presentationTimeOffset) {
+                                    r.segmentTemplate.presentationTimeOffset = offset * (r.segmentTemplate.timescale || 1);
+                                }
+                            });
+                        }
+                    });
+                }
+            } catch(e) {
+                shaka.log.warning('Attempt of fixing presentationTimeOffset in manifest failed. Trying to continue anyway.', e, period);
+            }
+        }.bind(this));
+    }
+    shaka.log.info('MPD', mpd);
+    return mpd;
+};
+
+/**
+ * This algorithm of finding the value to be used as the missing presentationTimeOffset has proven to work best in practical testing.
+ * @param {shaka.dash.mpd.Period} period
+ * @returns {number} The offset based on the video segment list, if relevant.
+ * @private
+ */
+shaka.vimond.dash.PreprocessableMpdRequest.prototype.findFirstOffsetWithVideo_ = function(period) {
+    'use strict';
+    var videoOffset = 0;
+    try {
+        period.adaptationSets.forEach(function(/** {shaka.dash.mpd.AdaptationSet} */ as) {
+            var st = as.segmentTemplate;
+            if (st && !st.presentationTimeOffset) {
+                var firstSegment = st.timeline && st.timeline.timePoints && st.timeline.timePoints[0];
+                if (firstSegment.startTime) {
+                    var offset = firstSegment.startTime / (st.timescale || 1);
+                    if (as.contentType === 'video') {
+                        videoOffset = offset;
+                    }
+                }
+            }
+        });
+    } catch(e) {
+        shaka.log.warning('Error when searching for start offset snapping to video segment start.', e);
+        return 0;
+    }
+    return videoOffset;
 };
