32cefb22bbbedfe42a73e0974487532f6f1257c6
Tor Erik Alræk <torerik@vimond.com>
2018-07-02 15:21:38 +0200

    CommonJS inclusion: Explicit global property window replacing the expectation of this === window.

--- build/wrapper.template.js
+++ build/wrapper.template.js
@@ -1,6 +1,6 @@
 (function(){var g={};
-(function(window){%output%}.bind(g,this))();
+(function(window){%output%}.bind(g,window))();
 if (typeof(module)!="undefined"&&module.exports)module.exports=g.shaka;
 else if (typeof(define)!="undefined" && define.amd)define(function(){return g.shaka});
-else this.shaka=g.shaka;
+else window.shaka=g.shaka;
 })();
